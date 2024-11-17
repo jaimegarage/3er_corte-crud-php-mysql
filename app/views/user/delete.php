ea7e350e0023ba2b3b9bdc558f671568056b45f1
Jaime Gaviria <72325369+jaimegarage@users.noreply.github.com>
2024-11-17 16:37:07 -0400

    Delete app directory

--- app/views/user/delete.php
+++
@@ -1,5 +0,0 @@
-<h1>Delete User</h1>
-<form action="delete.php?id=<?php echo $user['id']; ?>" method="POST">
-    <p>Are you sure you want to delete this user?</p>
-    <button type="submit">Delete</button>
-</form>
